6401754b02a3d8b07929bbd6b9393e363ed544bc
hexoul <crosien@gmail.com>
2021-09-25 22:34:52 +0900

    Localize store icons

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ module.exports = (phase, { defaultConfig }) => {
         env: {
             logoImg: 'https://raw.githubusercontent.com/hexoul/prepare-interview-data/master/logo.png',
             appstoreUrl: 'https://apps.apple.com/kr/app/id1526993862',
-            appstoreImg: 'https://apple-resources.s3.amazonaws.com/media-badges/download-on-the-app-store/black/en-us.svg',
+            appstoreImg: 'https://apple-resources.s3.amazonaws.com/media-badges/download-on-the-app-store/black/ko-kr.svg',
             playstoreUrl: 'https://play.google.com/store/apps/details?id=com.whatseries.prepareinterview',
-            playstoreImg: 'https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png',
+            playstoreImg: 'https://play.google.com/intl/en_us/badges/static/images/badges/ko_badge_web_generic.png',
         },
     }
     // console.log(commonConfig)
