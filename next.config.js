3e6451d8d93dd2e2ba221e47b6626e583f20809f
hexoul <crosien@gmail.com>
2021-09-25 11:05:34 +0900

    Include dotfiles in deploying

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
 module.exports = (phase, { defaultConfig }) => {
-    commonConfig = defaultConfig
-    commonConfig.basePath = '/prepare-interview-data'
+    commonConfig = {
+        ...defaultConfig,
+        basePath: '/prepare-interview-data',
+    }
     // console.log(commonConfig)
 
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
+            ...commonConfig,
             /* development only config options here */
-            ...commonConfig
         }
     }
 
     return {
+        ...commonConfig,
         /* config options for all phases except development here */
-        ...commonConfig
     }
 }
